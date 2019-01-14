5b3a7d556e6fb60cf2d77ac81ef5fa3d7de4e4b9
Niclas Hammar <niclas.hammar@frontedgeit.se>
2019-01-14 10:45:34 +0100

    Configure endpoint to get course by id

--- soap-course-management/src/main/java/com/hammar/webservices/soap/soapcoursemanagement/soap/CourseDetailsEndpoint.java
+++ soap-course-management/src/main/java/com/hammar/webservices/soap/soapcoursemanagement/soap/CourseDetailsEndpoint.java
@@ -1,5 +1,6 @@
 package com.hammar.webservices.soap.soapcoursemanagement.soap;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
@@ -8,19 +9,31 @@ import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 import com.hammar.courses.CourseDetails;
 import com.hammar.courses.GetCourseDetailsRequest;
 import com.hammar.courses.GetCourseDetailsResponse;
+import com.hammar.webservices.soap.soapcoursemanagement.soap.bean.Course;
+import com.hammar.webservices.soap.soapcoursemanagement.soap.service.CourseDetailService;
 
 @Endpoint
 public class CourseDetailsEndpoint {
 	
+	@Autowired
+	CourseDetailService service;
+	
 	@PayloadRoot(namespace="http://hammar.com/courses", localPart="GetCourseDetailsRequest")
 	@ResponsePayload
 	public GetCourseDetailsResponse processRequest(@RequestPayload GetCourseDetailsRequest request) {
-		GetCourseDetailsResponse response = new GetCourseDetailsResponse();
+		Course course = service.findById(request.getId());
 		
+		
+		return mapCourse(course);
+	}
+
+	private GetCourseDetailsResponse mapCourse(Course course) {
+		GetCourseDetailsResponse response = new GetCourseDetailsResponse();
 		CourseDetails cd = new CourseDetails();
-		cd.setId(request.getId());
-		cd.setName("Micro");
-		cd.setDescription("Fun with microservices");
+		
+		cd.setId(course.getId());
+		cd.setName(course.getName());
+		cd.setDescription(course.getDescription());
 		response.setCourseDetails(cd);
 		
 		return response;
---
+++ soap-course-management/src/main/java/com/hammar/webservices/soap/soapcoursemanagement/soap/service/CourseDetailService.java
@@ -0,0 +1,55 @@
+package com.hammar.webservices.soap.soapcoursemanagement.soap.service;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.hammar.webservices.soap.soapcoursemanagement.soap.bean.Course;
+
+@Component
+public class CourseDetailService {
+	
+	private static List<Course> courses = new ArrayList<>();
+	
+	static {
+		Course course1 = new Course(1, "Math", "Learn math");
+		courses.add(course1);
+		
+		Course course2 = new Course(2, "Cooking", "Learn cooking");
+		courses.add(course2);
+		
+		Course course3 = new Course(3, "History", "Learn history");
+		courses.add(course3);
+		
+		Course course4 = new Course(4, "Sports", "Learn sports");
+		courses.add(course4);
+	}
+	
+	public Course findById(int id) {
+		for(Course course: courses) {
+			if(course.getId() == id)
+				return course;
+		}
+		return null;
+	}
+	
+	public List<Course> findAll() {
+		return courses;
+	}
+	
+	public int deleteById(int id) {
+		Iterator<Course> iterator = courses.iterator();
+		while(iterator.hasNext()) {
+			Course course = iterator.next();
+			if(course.getId() == id) {
+				iterator.remove();
+				return 1;
+			}
+			
+		}
+		return 0;
+	}
+
+}
