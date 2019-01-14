ff33f6d61a651c6081db19c704d5924fb19129c9
Niclas Hammar <niclas.hammar@frontedgeit.se>
2019-01-14 15:42:12 +0100

    Enhanced create user to repond with 201 created

--- restful-web-service/src/main/java/com/hammar/rest/webservices/restfulwebservice/user/UserResource.java
+++ restful-web-service/src/main/java/com/hammar/rest/webservices/restfulwebservice/user/UserResource.java
@@ -1,13 +1,17 @@
 package com.hammar.rest.webservices.restfulwebservice.user;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
@@ -26,8 +30,16 @@ public class UserResource {
 	}
 
 	@PostMapping("/users")
-	public void createUser(@RequestBody User user) {
+	public ResponseEntity<Object> createUser(@RequestBody User user) {
 		User savedUser = dao.save(user);
+		
+		URI uri = ServletUriComponentsBuilder
+		.fromCurrentRequest()
+		.path("/{id}")
+		.buildAndExpand(savedUser.getId())
+		.toUri();
+		
+		return ResponseEntity.created(uri).build();
 	}
 
 }
